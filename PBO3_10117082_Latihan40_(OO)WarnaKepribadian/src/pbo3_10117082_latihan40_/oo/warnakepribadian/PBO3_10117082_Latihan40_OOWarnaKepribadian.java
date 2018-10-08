6dc5d8daaa5630ad9c8b05ad591551203410bdae
widiamka <43339745+widiamka@users.noreply.github.com>
2018-10-08 11:30:18 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan40_(OO)WarnaKepribadian/src/pbo3_10117082_latihan40_/oo/warnakepribadian/PBO3_10117082_Latihan40_OOWarnaKepribadian.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan40_.oo.warnakepribadian;
+import java.util.Scanner;
+
+/**
+ *
+ * Nama : WIDIAMEGA K
+* Kelas : IF3
+* NIM   : 10117082
+* Deskripsi Program : program ini berisi program untuk menampilkan kepribadian 
+* seseorang dari warna sesuai ketentuan dengan konsep oop
+ */
+public class PBO3_10117082_Latihan40_OOWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String warnaK;
+        String namaK;
+        Warna warni = new Warna();
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print(Warna.ANSI_RED + "YUK "
+                + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_GREEN + "CEK "
+                + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_YELLOW + "KEPRIBADIANMU "
+                + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_CYAN + "DARI "
+                + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_PURPLE + "WARNA "
+                + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_BLUE + "FAVORITMU "
+                + Warna.ANSI_RESET + "\n");
+
+        System.out.print("\n" + Warna.ANSI_RED_BACKGROUND + Warna.ANSI_WHITE
+                + "\t MERAH \t\t\n" + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_GREEN_BACKGROUND + Warna.ANSI_WHITE
+                + "\t HIJAU \t\t\n" + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_YELLOW_BACKGROUND + Warna.ANSI_WHITE
+                + "\t KUNING \t\n" + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_BLUE_BACKGROUND + Warna.ANSI_WHITE
+                + "\t BIRU \t\t\n" + Warna.ANSI_RESET);
+        System.out.print(Warna.ANSI_PURPLE_BACKGROUND + Warna.ANSI_WHITE
+                + "\t UNGU \t\t\n\n" + Warna.ANSI_RESET);
+
+        System.out.print("PILIH WARNA FAVORITMU : ");
+        warnaK = scn.next();
+        System.out.print("NAMA KAMU : ");
+        namaK = scn.next();
+
+        System.out.println("\n====HASIL TEST KEPRIBADIAN "
+                + namaK.toUpperCase() + "====");
+        Warna.hasilTest(warnaK);
+    }
+    
+}
